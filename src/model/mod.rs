d56aaafce7f5106a65a4151fb392f49d1f833391
Denux <timon.klinkert@denux.dev>
2023-11-08 13:21:32 +0100

    improved documentation

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -1,5 +1,20 @@
 //! Contains all the predefined models for the application.
 //! You can create your own models to customize the request.
-// TODO add more documentation
+//!
+//! # Supplying your own struct - example
+//! ```
+//! use serde::{Deserialize, Serialize};
+//! use ip_api4rs::IpApiClient;
+//!
+//! #[derive(Deserialize, Serialize)]
+//! struct Custom {
+//!    #[serde(rename = "query")]
+//!    ip: String,
+//!    country: String,
+//! }
+//!
+//!    let client = IpApiClient::new();
+//!    let result = client.query_api::<Custom>("8.8.8.8".to_string()).await;
+//! ```
 
 pub mod ip_response;
