ab3476f46a4553a571e44c7f14e218c93ef2d39b
Denux <timon.klinkert@denux.dev>
2023-11-26 21:54:30 +0100

    fixed clippy

--- src/blocking/client.rs
+++ src/blocking/client.rs
@@ -7,42 +7,39 @@ use crate::IpApiClient;
 
 /// A client for the ip-api.com API that blocks the current thread.
 struct BlockingIpApiClient {
-	/// The inner client to use for the requests.
-	pub inner_client: IpApiClient,
-	/// The runtime to use for the requests.
-	pub runtime: Runtime,
+    /// The inner client to use for the requests.
+    pub inner_client: IpApiClient,
+    /// The runtime to use for the requests.
+    pub runtime: Runtime,
 }
 
-impl Default for IpApiClient {
-	fn default() -> Self {
-		Self::new()
-	}
+impl Default for BlockingIpApiClient {
+    fn default() -> Self {
+        Self::new()
+    }
 }
 
 impl BlockingIpApiClient {
-	/// Creates a new IpApiClient with no API key.
-	pub fn new() -> Self {
-		Self {
-			inner_client: IpApiClient::new(),
-			runtime: Self::build_runtime(),
-		}
-	}
+    /// Creates a new IpApiClient with no API key.
+    pub fn new() -> Self {
+        Self {
+            inner_client: IpApiClient::new(),
+            runtime: Self::build_runtime(),
+        }
+    }
 
-	/// Creates a new IpApiClient with an API key.
-	pub fn new_with_api_key(api_key: String) -> Self {
-		Self {
-			inner_client: IpApiClient::new_with_api_key(api_key),
-			runtime: Self::build_runtime(),
-		}
-	}
+    /// Creates a new IpApiClient with an API key.
+    pub fn new_with_api_key(api_key: String) -> Self {
+        Self {
+            inner_client: IpApiClient::new_with_api_key(api_key),
+            runtime: Self::build_runtime(),
+        }
+    }
 
-	/// Builds the tokio runtime that will be used for the blocking requests.
-	fn build_runtime() -> Runtime {
-		return tokio::runtime::Builder::new_multi_thread()
-			.enable_all()
-			.build()
-			.unwrap();
-	}
+    /// Builds the tokio runtime that will be used for the blocking requests.
+    fn build_runtime() -> Runtime {
+        return tokio::runtime::Builder::new_multi_thread().enable_all().build().unwrap();
+    }
 
     /// Queries the API with the default fields.
     ///
@@ -52,7 +49,7 @@ impl BlockingIpApiClient {
     /// # Returns
     /// * `IpDefaultResponse` - The response from the API.
     pub fn query_api_default(&self, ip: &String) -> Result<IpDefaultResponse, IpApiError> {
-		return self.runtime.block_on(self.inner_client.query_api_default(ip));
+        return self.runtime.block_on(self.inner_client.query_api_default(ip));
     }
 
     /// Queries the API with all fields.
@@ -63,7 +60,7 @@ impl BlockingIpApiClient {
     /// # Returns
     /// * `IpFullResponse` - The response from the API.
     pub fn query_api_fully(&self, ip: &String) -> Result<IpFullResponse, IpApiError> {
-		return self.runtime.block_on(self.inner_client.query_api_fully(ip));
+        return self.runtime.block_on(self.inner_client.query_api_fully(ip));
     }
 
     /// Queries the API with a custom struct.
@@ -77,6 +74,6 @@ impl BlockingIpApiClient {
     where
         T: DeserializeOwned,
     {
-		return self.runtime.block_on(self.inner_client.query_api::<T>(ip));
+        return self.runtime.block_on(self.inner_client.query_api::<T>(ip));
     }
 }
